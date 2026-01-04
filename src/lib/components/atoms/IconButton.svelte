f0fec28e9e528de84d74ac72da2691075b2fadd1
Jan Komarek <jan.komarek@satoshilabs.com>
2026-01-04 21:19:24 +0100

    add touch-manipulation class to IconButton

--- src/lib/components/atoms/IconButton.svelte
+++ src/lib/components/atoms/IconButton.svelte
@@ -37,7 +37,7 @@
 	}));
 
 	const baseClasses =
-		'flex items-center justify-center focus:ring-2 focus:ring-blue-500 focus:outline-none disabled:opacity-50';
+		'flex items-center justify-center focus:ring-2 focus:ring-blue-500 focus:outline-none disabled:opacity-50 touch-manipulation';
 </script>
 
 <button
